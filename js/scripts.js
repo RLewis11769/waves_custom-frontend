343a964e921168035490f3266560a239878fbc4b
Rachel Lewis <RLewis11769@gmail.com>
2021-10-22 19:58:09 +0000

    Add comments to better explain js for future reference

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 	// Add validation to form on "contact" page
 	validateForm();
 
+	// Initialize methods for interacting with API
 	APPController.init();
 });
 
@@ -77,13 +78,16 @@ function validateForm() {
 
 
 const APIController = (function () {
-
+	// Client ID and secret ID assigned by Spotify to this app (these should be hidden...)
 	const clientId = '5010e2be878d4c8cb3c766e8e367431b';
 	const clientSecret = 'dc1997ba8b8042438914d7e8c2e662d9';
+	// Hardcoded ID for "Waves" playlist of 12 songs
 	const playlistID = '6MfkoiUr4P9iDLN5VZSvQN';
 
-	// private methods
+	// Private methods
 	const _getToken = async () => {
+		// Async method to pass client ID and secret to Spotify in return for token
+		// Specifically for using Client Credentials OAuth2 - only includes endpoints
 		const result = await fetch('https://accounts.spotify.com/api/token', {
 			method: 'POST',
 			headers: {
@@ -92,31 +96,37 @@ const APIController = (function () {
 			},
 			body: 'grant_type=client_credentials'
 		});
-
+		// Token is returned as object so need to jsonify and pull out token itself
 		const data = await result.json();
-		return data.access_token;
+		return (data.access_token);
 	}
 
 	const _getPlaylist = async (token) => {
+		// Async method to return data in playlist
+		// Pass token and will access object at this URL
 		const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}`, {
 			method: 'GET',
 			headers: { 'Authorization': 'Bearer ' + token }
 		});
 
 		const data = await result.json();
-		return data;
+		return (data);
 	}
 
 	const _getArtists = async (token, artistsIDs) => {
+		// Async method to return data in playlist
+		// Pass token and comma-separated list of artist IDs and will access object at this URL
+		// Note: Could also access artists/${ID} several times but responsible to call several at once
 		const result = await fetch(`https://api.spotify.com/v1/artists?ids=${artistsIDs}`, {
 			method: 'GET',
 			headers: { 'Authorization': 'Bearer ' + token }
 		});
 
 		const data = await result.json();
-		return data;
+		return (data);
 	}
 
+	// External interface to make private methods available outside of function
 	return {
 		getToken() {
 			return _getToken();
@@ -128,22 +138,23 @@ const APIController = (function () {
 			return _getArtists(token, artists_ids);
 		}
 	}
+	// IIFE - immediately-invoked function expression
 })();
 
 
 // UI Module
 const UIController = (function () {
+	// Client-side storage of token in HTML
+	// Note: Not at all connected to APICtrl()
 
-	const Token = '#hidden_token';
-
-	//public methods
+	// Public methods
 	return {
 		storeToken(token) {
-			document.querySelector(Token).value = token;
+			document.querySelector('#hidden_token').value = token;
 		},
 		getStoredToken() {
 			return {
-				token: document.querySelector(Token).value
+				token: document.querySelector('#hidden_token').value
 			}
 		}
 	}
@@ -151,68 +162,68 @@ const UIController = (function () {
 })();
 
 const APPController = (function (UICtrl, APICtrl) {
+	// Controller function that has access to methods in functions passed in
 
 	const loadPlaylist = async () => {
-		//get the token
+		// Get the token by accessing public method
 		const token = await APICtrl.getToken();
-		//store the token onto the page
+		// Store token on the page
 		UICtrl.storeToken(token);
+
+		// Get playlist
 		const playlist = await APICtrl.getPlaylist(token);
+		// Artists must be passed in as comma-separated string
 		let artists = '';
+		// Do two things for each track in playlist:
+		// 1. Add artist to artists string for slideshow on homepage
+		// 2. Add embedded track on gallery page
 		for (let track of playlist.tracks.items) {
 			artists += `${track.track.artists[0].id},`;
 			addEmbed(track.track.id)
 		}
 
-		artists = artists.slice(0, -1);
-		loadArtists(artists);
+		// Slice off final comma and send entire string to function
+		loadArtists(artists.slice(0, -1));
+	}
+
+	const loadArtists = async (artistsIDs) => {
+		// Get the token by accessing public method
+		const token = await APICtrl.getToken();
+		// Store token on the page
+		UICtrl.storeToken(token);
+
+		// Get array of artist objects
+		const artists = await APICtrl.getArtists(token, artistsIDs);
+		// Add data to page in loop
+		for (let artist of artists.artists) {
+			addArtistData(artist);
+		}
+		// Now that data is populated, slick carousel on homepage
+		$('#album').slick(singleCarousel);
 	}
 
 	function addEmbed(trackID) {
+		// Add iframe/30-sec embedded track for each track in playlist
 		$('#tracks').prepend(`<iframe src="https://open.spotify.com/embed/track/${trackID}"
 			width="300" height="380" frameborder="0"
 			allowtransparency="true" allow="encrypted-media">
 		</iframe>`)
 	}
 
-	function addAlbumData(data) {
-		console.log(data)
-		$('#tracks').prepend(`<div class="dark-bg col bord">
-			<img class="square card-img-top img-fluid pt-5" src="${data.album.images[0].url}" alt="">
-			<div class="card-body">
-				<h1 class="card-title lead font-weight-bold">${data.name}</h1>
-				<img class="rounded-circle" src="images/dark_wave.jpg" height="100rem" width="100rem"
-					alt="">
-				<h2 class="pt-2 mb-1 lead">${data.artists[0].name}</h2>
-				<p class="pt-2 mb-1">${data.album.name}</p>
-			</div>
-		</div>`)
-	}
-
 	function addArtistData(data) {
+		// Add data for each slide in slideshow
 		$('#album').prepend(`<div class="flex-column">
-            <img class="square mx-auto" src="${data.images[0].url}" alt="album cover">
-            <h3 class="py-4">${data.name}</h3>
-        </div>`);
-	}
-
-	const loadArtists = async (artistsIDs) => {
-		//get the token
-		const token = await APICtrl.getToken();
-		//store the token onto the page
-		UICtrl.storeToken(token);
-		const artists = await APICtrl.getArtists(token, artistsIDs);
-		for (let artist of artists.artists) {
-			addArtistData(artist);
-		}
-		$('#album').slick(singleCarousel);
+			<img class="square mx-auto" src="${data.images[0].url}" alt="album cover">
+			<h3 class="py-4">${data.name}</h3>
+		</div>`);
 	}
 
 	return {
+		// Public method driver program that can be accessed externally
 		init() {
-			console.log('App is starting');
+			// Enter loadPlaylist on initialization
 			loadPlaylist();
 		}
 	}
-
+	// UIController, APIController, and this APPController are all already called on initialization
 })(UIController, APIController);
